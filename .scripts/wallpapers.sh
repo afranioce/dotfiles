dbd917eeb211140c89553246ade67b44db935177
Afranio Martins <afranioce@gmail.com>
2021-09-12 12:18:41 -0300

    fix: wallpaper path

--- .scripts/wallpapers.sh
+++ .scripts/wallpapers.sh
@@ -4,10 +4,10 @@
 
 RESOLUTION="2560x1440"
 URL="https://source.unsplash.com/featured/${RESOLUTION}/?cyberpunk"
-WALLPAPER=/usr/share/backgrounds/gnome/Wallpaper.jpeg
+WALLPAPER=/home/$(whoami)/.wallpaper.jpeg
 
 RES=$(curl -L "$URL" -o "$WALLPAPER")
 if [ -z "$RES" ]; then
-    gsettings set org.gnome.desktop.background picture-uri "file:///$WALLPAPER"
-    gsettings set org.gnome.desktop.screensaver picture-uri "file:///$WALLPAPER"
+    gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER"
+    gsettings set org.gnome.desktop.screensaver picture-uri "file://$WALLPAPER"
 fi
